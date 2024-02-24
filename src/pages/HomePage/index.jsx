32b01ecdbe33a4071c0d301d61410f9d3f1f7f1f
luizaleblanc <luizaa.fq@gmail.com>
2024-02-24 20:39:10 -0300

    logical tasks

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,26 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
+import { API } from "../../services/Api";
 
 export const HomePage = () => {
-   const [productList, setProductList] = useState([]);
-   const [cartList, setCartList] = useState([]);
+  const [productList, setProductList] = useState([]);
+  const [cartList, setCartList] = useState([]);
+  const [isCartModal, setIsCartModal] = useState(false);
 
-   // useEffect montagem - carrega os produtos da API e joga em productList
-   // useEffect atualização - salva os produtos no localStorage (carregar no estado)
-   // adição, exclusão, e exclusão geral do carrinho
-   // renderizações condições e o estado para exibir ou não o carrinho
-   // filtro de busca
-   // estilizar tudo com sass de forma responsiva
+  // useEffect montagem - carrega os produtos da API e joga em productList (FEITO)
+  // useEffect atualização - salva os produtos no localStorage (carregar no estado)
+  // adição, exclusão, e exclusão geral do carrinho
+  // renderizações condições e o estado para exibir ou não o carrinho
+  // filtro de busca
+  // estilizar tudo com sass de forma responsiva
 
-   return (
-      <>
-         <Header />
-         <main>
-            <ProductList productList={productList} />
-            <CartModal cartList={cartList} />
-         </main>
-      </>
-   );
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const { data } = await API.get("/products");
+
+        setProductList(data);
+      } catch (error) {
+        console.error("Erro ao obter os produtos:", error);
+      }
+    };
+
+    getProducts();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <ProductList productList={productList} />
+
+        {isCartModal && <CartModal cartList={cartList} setIsOpen={setIsCartModal} />}
+
+        <button onClick={() => setIsCartModal(true)}>Abrir modal</button>
+      </main>
+    </>
+  );
 };
